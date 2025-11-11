735a24aa0bcb4c970cac405df364be127bcfdf59
DESKTOP-F65HMQ0\Nathan <nathan.scullen93@gmail.com>
2025-11-11 15:51:32 +1000

    Completed CH3 L7 now can upload videos to S3 bucket

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,81 @@
 import { respondWithJSON } from "./json";
 
 import { type ApiConfig } from "../config";
-import type { BunRequest } from "bun";
+import { s3, write, type BunRequest, type S3File } from "bun";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { getBearerToken, validateJWT } from "../auth";
+import { randomBytes, type UUID } from "crypto";
+import { getVideo, updateVideo } from "../db/videos";
+import path from "path";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
+  const { videoId } = req.params as { videoId?: UUID };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  console.log("Uploading video file for video", videoId, "by user", userID);
+
+  const videoFile = (await req.formData()).get("video");
+  if (!videoFile || !(videoFile instanceof File)) {
+    throw new BadRequestError("video is required");
+  }
+
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    console.log("Video upload failed: video not found");
+    throw new NotFoundError("Couldn't find video");
+  }
+
+  if (video.userID !== userID) {
+    console.log("Video upload failed: user forbidden");
+    throw new UserForbiddenError(
+      "You do not have permission to access this video"
+    );
+  }
+
+  if (videoFile.type !== "video/mp4") {
+    console.log("Video upload failed: invalid file type");
+    throw new BadRequestError("Video must be a mp4 file");
+  }
+
+  const MAX_UPLOAD_SIZE = 1 * 1024 * 1024 * 1024; // 1 GB
+
+  if (videoFile.size > MAX_UPLOAD_SIZE) {
+    console.log("Video upload failed: file size exceeds limit");
+    throw new BadRequestError("Video file size exceeds the maximum limit");
+  }
+
+  //Save the uploaded video file to temp local storage
+  const data = await videoFile.arrayBuffer();
+  let randBytes = randomBytes(32).toString("base64url"); // Use this to generate a random filename therefore avoiding the need for caching
+  const fileExtension = "mp4";
+  const localPath = path.join(cfg.assetsRoot, `${randBytes}.${fileExtension}`);
+
+  await Bun.write(localPath, data);
+
+  //Copy the file to s3 storage
+  const metadata = s3.file(`${randBytes}.${fileExtension}`);
+
+  try {
+    await metadata.write(Bun.file(localPath), {
+      type: "video/mp4",
+    });
+    //Remove the temp video file after processing
+    await Bun.file(localPath).delete();
+
+//Update the videoURL in the database
+  const s3Url = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${randBytes}.${fileExtension}`;
+  video.videoURL = s3Url;
+  updateVideo(cfg.db, video);
+
+  } catch (error) {
+    console.log("Video upload failed: S3 upload error", error);
+    throw new Error("Failed to upload video to storage");
+  }
+
   return respondWithJSON(200, null);
 }
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 import { newDatabase } from "./db/db";
 import type { Database } from "bun:sqlite";
+import { s3, S3Client } from "bun";
 
 export type ApiConfig = {
   db: Database;
@@ -11,6 +12,9 @@ export type ApiConfig = {
   s3Region: string;
   s3CfDistribution: string;
   port: string;
+  accessKeyId: string;
+  s3SecretAccessKey?: string;
+  s3Client?: S3Client;
 };
 
 const pathToDB = envOrThrow("DB_PATH");
@@ -22,6 +26,8 @@ const s3Bucket = envOrThrow("S3_BUCKET");
 const s3Region = envOrThrow("S3_REGION");
 const s3CfDistribution = envOrThrow("S3_CF_DISTRO");
 const port = envOrThrow("PORT");
+const s3AccessKeyId = envOrThrow("AWS_ACCESS_KEY_ID");
+const s3SecretAccessKey = envOrThrow("AWS_SECRET_ACCESS_KEY");
 
 const db = newDatabase(pathToDB);
 
@@ -35,6 +41,14 @@ export const cfg: ApiConfig = {
   s3Region: s3Region,
   s3CfDistribution: s3CfDistribution,
   port: port,
+  accessKeyId: s3AccessKeyId,
+  s3SecretAccessKey: s3SecretAccessKey,
+  s3Client: new S3Client({
+    bucket: s3Bucket,
+    region: s3Region,
+    accessKeyId: s3AccessKeyId,
+    secretAccessKey: s3SecretAccessKey,
+  }),
 };
 
 function envOrThrow(key: string) {
